c50848d18bfc90d36b2ac2644527f786f9d0d518
kurs <kurs@DESKTOP-284UO99>
2024-10-25 10:19:08 +0200

    q

--- P04ZadnieTablice/Program.cs
+++ P04ZadnieTablice/Program.cs
@@ -17,34 +17,56 @@ namespace P04ZadnieTablice
             string znakKoncowy = ">";
             string sciezka = @"C:\dane\miasta.txt";
 
-            string[] miastaZPliku = File.ReadAllLines(sciezka);
-
-
-            Console.WriteLine("Dostepne miasta:");
-            for (int i = 0;i<miastaZPliku.Length;i++)
-                Console.WriteLine($"[{i+1}] {miastaZPliku[i]}");
-
-            while (true) 
+            string[] miastaZPliku = null;
+            try
             {
-                int nrMiasta = Convert.ToInt32(Console.ReadLine());
-                string miasto = miastaZPliku[nrMiasta - 1];
-
-                string adres = adresPodstawowy + miasto;
-
-                WebClient wc = new WebClient();
-                string dane = wc.DownloadString(adres);
-
-                int indx = dane.IndexOf(szukanyZnak);
-                int aktualnaPozycja = indx;
-
-                while (dane.Substring(aktualnaPozycja, 1) != znakKoncowy)
-                    aktualnaPozycja--;
-
-                string wynik = dane.Substring(aktualnaPozycja + 1, indx - aktualnaPozycja + 1);
-
-                Console.WriteLine($"Temeratura dla miasta {miasto} wynosi {wynik}");
+                miastaZPliku = File.ReadAllLines(sciezka);
             }
-            
+            catch (Exception ex)
+            {
+                Console.WriteLine("Nie udało się wczytac pliku");
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+              //  throw ex;
+            }
+
+            if (miastaZPliku != null)
+            {
+
+                Console.WriteLine("Dostepne miasta:");
+                for (int i = 0; i < miastaZPliku.Length; i++)
+                    Console.WriteLine($"[{i + 1}] {miastaZPliku[i]}");
+
+                while (true)
+                {
+                    try
+                    {
+                        int nrMiasta = Convert.ToInt32(Console.ReadLine());
+                        string miasto = miastaZPliku[nrMiasta - 1];
+
+                        string adres = adresPodstawowy + miasto;
+
+                        WebClient wc = new WebClient();
+                        string dane = wc.DownloadString(adres);
+
+                        int indx = dane.IndexOf(szukanyZnak);
+                        int aktualnaPozycja = indx;
+
+                        while (dane.Substring(aktualnaPozycja, 1) != znakKoncowy)
+                            aktualnaPozycja--;
+
+                        string wynik = dane.Substring(aktualnaPozycja + 1, indx - aktualnaPozycja + 1);
+
+                        Console.WriteLine($"Temeratura dla miasta {miasto} wynosi {wynik}");
+                    }
+                    catch (Exception ex)
+                    {
+                        File.AppendAllText(@"c:\dane\errors\errlog.txt", Environment.NewLine+DateTime.Now + " " + ex.Message);
+                        Console.WriteLine("Nie udało się pobrać tempratury");
+                    }
+                   
+                }
+            }
 
         }
     }
---
+++ P05ObslugaBledow/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05ObslugaBledow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 0;
+
+            string s = "ala";
+
+
+            try
+            {
+                int c = a / b;
+                string s1 = s.Substring(3,1);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Podales bledny index");
+                File.AppendAllText(@"c:\dane\erros\errorlog.txt", Environment.NewLine + DateTime.Now + " " + ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Nie mozesz dzielić przez zero");
+                File.AppendAllText(@"c:\dane\erros\errorlog.txt", Environment.NewLine + DateTime.Now + " " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Coś poszło nie tak");
+                File.AppendAllText(@"c:\dane\erros\errorlog.txt", Environment.NewLine + DateTime.Now + " " + ex.Message);
+            }
+           
+            Console.ReadKey (); 
+
+        }
+    }
+}
