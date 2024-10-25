c700d6a795c39752811a84ff79960897c303cf80
kurs <kurs@DESKTOP-284UO99>
2024-10-25 10:31:09 +0200

    q

---
+++ P06KonwersjaTablicOrazNapisow/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P06KonwersjaTablicOrazNapisow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string zdanie = "ala ma kota";
+
+            // SPLIT  - rozdzialenie
+
+            string[] wyrazy = zdanie.Split(' ');
+
+            string zdanie2 = "ala!$ma**kota!i ma psa";
+
+            string[] separatory = { "!$", "**", "!" };
+
+            string[] wyrazy2 = zdanie2.Split(separatory, StringSplitOptions.RemoveEmptyEntries);
+
+            // JOIN - łączenie
+
+
+            string[] tablica = { "jan", "ola", "adam" };
+
+            string wynik = string.Join(" - ", tablica);
+            Console.WriteLine(wynik);
+            Console.ReadKey();
+        }
+    }
+}
