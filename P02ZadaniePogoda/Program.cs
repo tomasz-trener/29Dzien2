87fb325cb5479e1463782bbfe9cebf4b3d694712
kurs <kurs@DESKTOP-284UO99>
2024-10-25 08:50:50 +0200

    q

--- P02ZadaniePogoda/Program.cs
+++ P02ZadaniePogoda/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,30 @@ namespace P02ZadaniePogoda
     {
         static void Main(string[] args)
         {
+            string adresPodstawowy = "https://www.google.com/search?q=pogoda ";
+            string szukanyZnak = "°";
+            string znakKoncowy = ">";
+
+            while (true) 
+            {
+                Console.WriteLine("Podaj nazwe miasta");
+                string miasto = Console.ReadLine();
+                string adres = adresPodstawowy + miasto;
+
+                WebClient wc = new WebClient();
+                string dane = wc.DownloadString(adres);
+
+                int indx = dane.IndexOf(szukanyZnak);
+                int aktualnaPozycja = indx;
+
+                while (dane.Substring(aktualnaPozycja, 1) != znakKoncowy)
+                    aktualnaPozycja--;
+
+                string wynik = dane.Substring(aktualnaPozycja + 1, indx - aktualnaPozycja + 1);
+                Console.WriteLine(wynik);
+            }
+           
+            
         }
     }
 }
