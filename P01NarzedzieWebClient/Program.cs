e4d8ce80399180158865d66a28e38a47025d7973
kurs <kurs@DESKTOP-284UO99>
2024-10-25 08:29:26 +0200

    Add project files.

---
+++ P01NarzedzieWebClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01NarzedzieWebClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient wc = new WebClient();
+            string dane = wc.DownloadString("https://www.google.com/search?q=pogoda+warszawa");
+
+            Console.WriteLine(dane);
+
+
+            File.WriteAllText("c:\\dane\\strona.html", dane);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ P02ZadaniePogoda/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("P02ZadaniePogoda")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("P02ZadaniePogoda")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b58fe9d1-f966-4eaa-a12d-1a84c250cba0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
