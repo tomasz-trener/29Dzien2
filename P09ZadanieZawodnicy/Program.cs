feb7f22083ba72a99ccbb0daac25ce604ee017c4
kurs <kurs@DESKTOP-284UO99>
2024-10-25 12:06:06 +0200

    q

--- P09ZadanieZawodnicy/Program.cs
+++ P09ZadanieZawodnicy/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P09ZadanieZawodnicy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wczytaj kraj");
+            string kraj = Console.ReadLine().ToLower();
+
+            string sciezka = "http://tomaszles.pl/wp-content/uploads/2019/06/zawodnicy.txt";
+
+            WebClient wc = new WebClient();
+            string dane = wc.DownloadString(sciezka);
+
+            string[] separatorWierszy = { "\r\n" };
+            string[] wiersze =dane.Split(separatorWierszy, StringSplitOptions.RemoveEmptyEntries);
+
+            List<string> zawodnicy = new List<string>();
+            for (int i = 1; i < wiersze.Length; i++)
+            {
+                string[] komorki = wiersze[i].Split(';');
+                if (komorki[4].ToLower() == kraj)
+                    zawodnicy.Add(komorki[2] + " " + komorki[3]);
+            }
+
+            string sciezkaWynik = @"c:\dane\kraje";
+            if(!Directory.Exists(sciezkaWynik))
+                Directory.CreateDirectory(sciezkaWynik);
+
+
+            if(!File.Exists(sciezkaWynik + $@"\{kraj}.txt"))
+                File.WriteAllLines(sciezkaWynik + $@"\{kraj}.txt", zawodnicy);
+        }
+    }
+}
