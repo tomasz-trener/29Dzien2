29f905e5d787b22aac15cc55b8f0dc38a998ef0a
kurs <kurs@DESKTOP-284UO99>
2024-10-25 14:10:15 +0200

    q

--- P11ZadanieTabliceWielowymiarowe/Program.cs
+++ P11ZadanieTabliceWielowymiarowe/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,45 @@ namespace P11ZadanieTabliceWielowymiarowe
     {
         static void Main(string[] args)
         {
+            string sciezka = "http://tomaszles.pl/wp-content/uploads/2019/06/zawodnicy.txt";
+
+            WebClient wc = new WebClient();
+            string dane = wc.DownloadString(sciezka);
+
+            string[] tabSep = { Environment.NewLine };
+            string[] wiersze = dane.Split(tabSep, StringSplitOptions.RemoveEmptyEntries);
+
+            int liczbaWierszy = wiersze.Length - 1;
+            int liczbaKolumn = wiersze[0].Split(';').Length;
+
+            string[,] tab = new string[liczbaWierszy, liczbaKolumn];
+
+            //wczytywanie danych 
+            for (int i = 1; i < wiersze.Length; i++)
+            {
+                string[] komorki = wiersze[i].Split(';');
+                for (int j = 0; j < komorki.Length; j++)
+                    tab[i-1,j] = komorki[j];
+            }
+
+            Console.WriteLine("--- sposob 1 ----");
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j++)
+                    Console.Write(tab[i,j] + " ");
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("--- sposob 1 ----");
+            for (int i = 0; i < tab.GetLength(1); i++)
+            {
+                for (int j = 0; j < tab.GetLength(0); j++)
+                    Console.Write(tab[j,i] + " ");
+
+                Console.WriteLine();
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ P12TabliceTablic/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12TabliceTablic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tablicaJednowymiarowa = new string[3];
+            string[,] tablicaDwuwymiarowa = new string[3, 2];
+
+            //ang. Jagged array (tablica postrzępiona , tablica tablic) 
+
+            string[][] tablicaTablic = new string[4][];
+
+            tablicaTablic[0] = new string[2];
+            tablicaTablic[1] = new string[3] {"x","y","z"};
+            tablicaTablic[2] = new string[] { "u", "k" };
+
+            tablicaTablic[0][0] = "a";
+            tablicaTablic[0][1] = "b";
+
+            int dlugosc = tablicaTablic.Length; // 4
+
+            int dlugoscKonkretnejTablicyWewnetrznej = tablicaTablic[1].Length; // 3 
+        }
+    }
+}
