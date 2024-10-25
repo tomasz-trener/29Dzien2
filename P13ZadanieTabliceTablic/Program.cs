4b3065560ace7939bfd87d74ac5968af2ee2906a
kurs <kurs@DESKTOP-284UO99>
2024-10-25 14:22:45 +0200

    q

--- P13ZadanieTabliceTablic/Program.cs
+++ P13ZadanieTabliceTablic/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,35 @@ namespace P13ZadanieTabliceTablic
     {
         static void Main(string[] args)
         {
+            string sciezka = "http://tomaszles.pl/wp-content/uploads/2019/06/zawodnicy.txt";
+
+            WebClient webClient = new WebClient();
+            string dane = webClient.DownloadString(sciezka);
+
+            string[] tabSep = {Environment.NewLine};
+            string[] wiersze= dane.Split(tabSep, StringSplitOptions.RemoveEmptyEntries);
+
+            int liczbaWierszy = wiersze.Length - 1;
+            string[][] tab = new string[liczbaWierszy][];
+
+            for (int i = 1; i <= liczbaWierszy; i++)
+                tab[i - 1] = wiersze[i].Split(';');
+
+            //wyswietlanie z zastosoawniem join
+            foreach (var zw in tab)
+                Console.WriteLine(string.Join(" ",zw));
+
+            // wyswietlenie bez join 
+            foreach (var zw in tab)
+            {
+                foreach (var k in zw)
+                    Console.Write(k + " ");
+
+                Console.WriteLine();
+            }
+
+
+            Console.ReadKey ();
         }
     }
 }
