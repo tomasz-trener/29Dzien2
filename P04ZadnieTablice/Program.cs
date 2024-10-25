12b8047f7209b9247facd814e7b1b78fca7560e4
kurs <kurs@DESKTOP-284UO99>
2024-10-25 09:49:07 +0200

    q

--- P04ZadnieTablice/Program.cs
+++ P04ZadnieTablice/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,40 @@ namespace P04ZadnieTablice
     {
         static void Main(string[] args)
         {
+            string adresPodstawowy = "https://www.google.com/search?q=pogoda ";
+            string szukanyZnak = "°";
+            string znakKoncowy = ">";
+            string sciezka = @"C:\dane\miasta.txt";
+
+            string[] miastaZPliku = File.ReadAllLines(sciezka);
+
+
+            Console.WriteLine("Dostepne miasta:");
+            for (int i = 0;i<miastaZPliku.Length;i++)
+                Console.WriteLine($"[{i+1}] {miastaZPliku[i]}");
+
+            while (true) 
+            {
+                int nrMiasta = Convert.ToInt32(Console.ReadLine());
+                string miasto = miastaZPliku[nrMiasta - 1];
+
+                string adres = adresPodstawowy + miasto;
+
+                WebClient wc = new WebClient();
+                string dane = wc.DownloadString(adres);
+
+                int indx = dane.IndexOf(szukanyZnak);
+                int aktualnaPozycja = indx;
+
+                while (dane.Substring(aktualnaPozycja, 1) != znakKoncowy)
+                    aktualnaPozycja--;
+
+                string wynik = dane.Substring(aktualnaPozycja + 1, indx - aktualnaPozycja + 1);
+
+                Console.WriteLine($"Temeratura dla miasta {miasto} wynosi {wynik}");
+            }
+            
+
         }
     }
 }
